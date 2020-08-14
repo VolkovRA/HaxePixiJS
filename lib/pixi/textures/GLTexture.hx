c6144eb6e1050e7db025ffc61071f02072ed264c
VolkovRA <flash21.ru@gmail.com>
2020-08-14 14:52:05 +0300

    ADD GLTexture

---
+++ lib/pixi/textures/GLTexture.hx
@@ -0,0 +1,63 @@
+package pixi.textures;
+
+import js.html.webgl.Texture;
+
+/**
+ * Internal texture for WebGL context.
+ * 
+ * @see Documentation: http://pixijs.download/release/docs/PIXI.GLTexture.html
+ * @see Source code: http://pixijs.download/release/docs/packages_core_src_textures_GLTexture.js.html
+ */
+@:native("PIXI.GLTexture")
+extern class GLTexture
+{
+    /**
+     * Create a new GLTexture instance.
+     */
+    public function new();
+
+    /**
+     * Texture contents dirty flag.
+     */
+    public var dirtyId:Int;
+
+    /**
+     * Texture style dirty flag.
+     */
+    public var dirtyStyleId:Int;
+
+    /**
+     * Width of texture that was used in texImage2D.
+     */
+    public var width:Int;
+
+    /**
+     * Height of texture that was used in texImage2D.
+     */
+    public var height:Int;
+
+    /**
+     * Type copied from baseTexture.
+     */
+    public var internalFormat:Type;
+
+    /**
+     * Whether mip levels has to be generated.
+     */
+    public var mipmap:Bool;
+
+    /**
+     * The WebGL texture.
+     */
+    public var texture:Texture;
+
+    /**
+     * Type copied from baseTexture.
+     */
+    public var type:Type;
+
+    /**
+     * WrapMode copied from baseTexture.
+     */
+    public var wrapMode:WrapMode;
+}
