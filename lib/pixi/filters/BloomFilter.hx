0ba6b04f2f8e8eb592c2b84f474bffdf4fc6fdd8
VolkovRA <flash21.ru@gmail.com>
2020-08-15 13:49:12 +0300

    ADD BloomFilter, ColorReplaceFilter

---
+++ lib/pixi/filters/BloomFilter.hx
@@ -0,0 +1,47 @@
+package pixi.filters;
+
+import haxe.extern.EitherType;
+import pixi.geom.Point;
+
+/**
+ * The BloomFilter applies a Gaussian blur to an object. The strength
+ * of the blur can be set for x- and y-axis separately.
+ * @see Documentation: https://pixijs.io/pixi-filters/docs/PIXI.filters.BloomFilter.html
+ * @see Source code: https://pixijs.io/pixi-filters/docs/filters_bloom_src_BloomFilter.js.html
+ * ***
+ * Library: **filters** 
+ */
+@:native("PIXI.filters.BloomFilter")
+extern class BloomFilter extends Filter
+{
+    /**
+     * Create a new BloomFilter instance.
+     * @param blur Sets the strength of both the blurX and blurY properties simultaneously.  
+     *             Default: `2`
+     * @param quality The quality of the blurX & blurY filter.  
+     *                Default: `4`
+     * @param resolution The resolution of the blurX & blurY filter.  
+     *                   Default: `Settings.RESOLUTION`
+     * @param kernelSize The kernelSize of the blurX & blurY filter.Options: 5, 7, 9, 11, 13, 15.  
+     *                   Default: `5`
+     */
+    function new(?blur:EitherType<Float,EitherType<Point,Array<Float>>>, ?quality:Float, ?resolution:Float, ?kernelSize:Float);
+
+    /**
+     * Sets the strength of both the blurX and blurY properties simultaneously.  
+     * Default: `2`
+     */
+    public var blur:Float;
+
+    /**
+     * Sets the strength of the blurX property.  
+     * Default: `2`
+     */
+    public var blurX:Float;
+
+    /**
+     * Sets the strength of the blurY property.  
+     * Default: `2`
+     */
+    public var blurY:Float;
+}
---
+++ lib/pixi/filters/ColorReplaceFilter.hx
@@ -0,0 +1,46 @@
+package pixi.filters;
+
+import haxe.extern.EitherType;
+
+/**
+ * ColorReplaceFilter, originally by mishaa, updated by timetocode
+ * http://www.html5gamedevs.com/topic/10640-outline-a-sprite-change-certain-colors/?p=69966
+ * 
+ * @see Documentation: https://pixijs.io/pixi-filters/docs/PIXI.filters.ColorReplaceFilter.html
+ * @see Source code: https://pixijs.io/pixi-filters/docs/filters_color-replace_src_ColorReplaceFilter.js.html
+ * ***
+ * Library: **filters** 
+ */
+@:native("PIXI.filters.ColorReplaceFilter")
+extern class ColorReplaceFilter extends Filter
+{
+    /**
+     * Create a new ColorReplaceFilter instance.
+     * @param originalColor The color that will be changed, as a 3 component RGB e.g. [1.0, 1.0, 1.0].  
+     *                      Default: `0xFF0000`
+     * @param newColor The resulting color, as a 3 component RGB e.g. [1.0, 0.5, 1.0].  
+     *                 Default: `0x000000`
+     * @param epsilon Tolerance/sensitivity of the floating-point comparison between colors (lower = more exact, higher = more inclusive)  
+     *                Default: `0.4`
+     */
+    function new(?originalColor:EitherType<Int,Array<Float>>, ?newColor:EitherType<Int,Array<Float>>, ?epsilon:Float);
+
+    /**
+     * Tolerance/sensitivity of the floating-point comparison between
+     * colors (lower = more exact, higher = more inclusive)  
+     * Default: `0.4`
+     */
+    public var epsilon:Float;
+
+    /**
+     * The resulting color, as a 3 component RGB e.g. [1.0, 0.5, 1.0]  
+     * Default: `0x000000`
+     */
+    public var newColor:EitherType<Int,Array<Float>>;
+
+    /**
+     * The color that will be changed, as a 3 component RGB e.g. [1.0, 1.0, 1.0]  
+     * Default: `0xFF0000`
+     */
+    public var originalColor:EitherType<Int,Array<Float>>;
+}
