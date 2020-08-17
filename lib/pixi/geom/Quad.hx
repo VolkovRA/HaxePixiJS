07e50e0ba1eccda9c5bcb296cc1d6a25d1d22b10
VolkovRA <flash21.ru@gmail.com>
2020-08-17 14:20:58 +0300

    ADD Quad

---
+++ lib/pixi/geom/Quad.hx
@@ -0,0 +1,18 @@
+package pixi.geom;
+
+/**
+ * Helper class to create a quad.
+ * 
+ * @see Documentation: http://pixijs.download/release/docs/PIXI.Quad.html
+ * @see Source code: http://pixijs.download/release/docs/packages_core_src_utils_Quad.js.html
+ * ***
+ * Library: **core** 
+ */
+@:native("PIXI.Quad")
+extern class Quad extends Geometry
+{
+    /**
+     * Create a new Quad instance.
+     */
+    public function new();
+}
